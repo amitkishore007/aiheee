e2b83588bfa94951eda7d22f4e4f18c0b1b9366b
amit kishore <kishoreamit5@gmail.com>
2017-05-08 13:05:01 +0530

    aiheee form init

---
+++ includes/config.php
@@ -0,0 +1,37 @@
+<?php  
+
+
+
+define('SITE_MODE',0);//zero means development other num means production
+
+
+if(SITE_MODE===0){
+
+ defined("DBHOST") ? null : define("DBHOST", "localhost");
+
+ defined("DBUSER") ? null : define("DBUSER", "root");
+
+ defined("DBNAME") ? null : define("DBNAME", "aiheee");
+
+ defined("DBPASS") ? null : define("DBPASS", "");
+
+}else{
+
+defined("DBHOST") ? null : define("DBHOST", "localhost");
+
+defined("DBUSER") ? null : define("DBUSER", "aiheee_shashank");
+
+defined("DBNAME") ? null : define("DBNAME", "aiheee_aiheee");
+
+defined("DBPASS") ? null : define("DBPASS", "Support@123");
+
+
+}
+
+$db = mysql_connect(DBHOST,DBUSER,DBPASS) ;
+	if(!$db){
+	die("unable to connect to database");
+}
+
+mysql_select_db(DBNAME,$db);
+?>
